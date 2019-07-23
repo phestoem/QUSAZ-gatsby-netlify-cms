ac3b6fe80f4343739c9d260faabe107455adec75
Phesto Mwakyusa <phesto@qusaz.com>
2019-07-24 01:31:12 +0300

    Update Navbar.js
    Change navbar items
    Point github to exact project

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import logo from '../img/logo.png'
 
 const Navbar = class extends React.Component {
   constructor(props) {
@@ -42,7 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+              <img src={logo} alt="QUSAZ" style={{ width: '88px' }} />
             </Link>
             {/* Hamburger menu */}
             <div
@@ -72,14 +72,12 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/contact">
                 Contact
               </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
-              </Link>
+             
             </div>
             <div className="navbar-end has-text-centered">
               <a
                 className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
+                href="https://github.com/phestoem/QUSAZ-gatsby-netlify-cms"
                 target="_blank"
                 rel="noopener noreferrer"
               >
